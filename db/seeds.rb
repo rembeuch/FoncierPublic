e31cfd4b28eb101a2640492972b67741beac9996
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-11 17:31:01 +0100

    add users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 require 'open-uri'
 
@@ -15,18 +8,14 @@ Reservation.destroy_all if Rails.env.development?
 
 
 puts "Create users"
-# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-# ingredients = JSON.parse(open(url).read)
-# ingredients["drinks"].each do |ingredient|
-#   i = Ingredient.create(name: ingredient["strIngredient1"])
-#   puts "create #{i.name}"
 100.times do
   User.create(
     first_name: Faker::Name.unique.first_name,
     last_name: Faker::Name.unique.last_name,
     email: Faker::Internet.email,
-    password: Faker::Lorem.paragraph
+    password: Faker::Internet.password,
+    description: Faker::Lorem.paragraph,
+    avatar: "dbr7upry2jb7eulcd0mz"
     )
 end
 
-end
