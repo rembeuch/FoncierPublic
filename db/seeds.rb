cb500fd1ede002fc193bdfb19382a9a7943e26f6
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-12 16:10:09 +0100

    added unspash to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 require 'open-uri'
 status = ['Accepté', 'Refusé', 'En attente' ]
-url_flat = "https://res.cloudinary.com/cbosseman/image/upload/v1573219418/bb9c0a8bhb0cshvjiyl2.jpg"
+url_flat = "https://source.unsplash.com/random/600x400"
 url_avatar = "https://res.cloudinary.com/cbosseman/image/upload/v1573492633/52eabf633ca6414e60a7677b0b917d92-male-avatar-maker_fnq3vb.jpg"
 
 puts "Destroy all instances"
@@ -11,7 +11,7 @@ User.destroy_all if Rails.env.development?
 
 
 puts "Create users"
-20.times do
+10.times do
   user = User.new(
     first_name: Faker::Name.unique.first_name,
     last_name: Faker::Name.unique.last_name,
@@ -23,24 +23,24 @@ puts "Create users"
   user.save
 end
 
-20.times do
+10.times do
   flat = Flat.new(
     title: Faker::Nation.capital_city,
     address: Faker::Address.street_address,
     description: Faker::Lorem.paragraph,
-    price_per_day: Faker::Number.between(from: 20, to: 300),
+    price_per_day: Faker::Number.between(from: 10, to: 300),
     owner: User.all.sample
     )
   flat.remote_photo_url = url_flat
   flat.save
 end
 
-20.times do
+10.times do
   Reservation.create(
     start_date: Date.today,
     end_date: Faker::Date.between(from: Date.today, to: 1.year.from_now),
     status: status.sample,
-    price: Faker::Number.between(from: 20, to: 300),
+    price: Faker::Number.between(from: 10, to: 300),
     flat: Flat.all.sample,
     guest: User.all.sample
     )
