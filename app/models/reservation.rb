189e11a7bdb6bd5ef05717154c30a58408d9bb4b
Corentin Bosseman <corentin.bosseman@gmail.com>
2019-11-12 15:26:11 +0100

    finishing adding reservations logic to index and new

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :flat
   belongs_to :guest, class_name: "User"
 
+  def set_total_price!
+    self.price = (end_date - start_date).to_i * flat.price_per_day
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_11_150436) do
+ActiveRecord::Schema.define(version: 2019_11_12_141843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_11_150436) do
   create_table "reservations", force: :cascade do |t|
     t.date "start_date"
     t.date "end_date"
-    t.string "status"
+    t.string "status", default: "En attente"
     t.integer "price"
     t.bigint "flat_id"
     t.bigint "guest_id"
