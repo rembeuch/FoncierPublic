b10503f1923264c0ec4227cc77518cc9a66031e8
Laszlo Van Daal <laszlo.vandaal@gmail.com>
2019-11-13 15:17:12 +0100

    add avatar

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,9 @@
+module AvatarHelper
+  def current_user_avatar(css_class: '')
+    if current_user.avatar.present?
+      cl_image_tag current_user.avatar, height: 100, width: 100, crop: :fill, class: css_class
+    else
+      image_tag "avatar_default.png", alt: "alttext", class: css_class
+    end
+  end
+end
