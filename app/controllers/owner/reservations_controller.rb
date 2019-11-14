85ac2375df87d63c730a7e388eb18cd531cc6cba
Remi Beucherie <beucherie.r@gmail.com>
2019-11-14 12:42:36 +0100

    add accept and decline action fow owner reservation

--- app/controllers/owner/reservations_controller.rb
+++ app/controllers/owner/reservations_controller.rb
@@ -2,4 +2,18 @@ class Owner::ReservationsController < ApplicationController
   def index
     @reservations = Reservation.joins(:flat).where(flats: { owner: current_user })
   end
+
+  def accept
+      @reservation = Reservation.find(params[:id])
+      @reservation.status = "acceptée"
+      @reservation.save
+      redirect_to owner_reservations_path
+  end
+
+  def decline
+      @reservation = Reservation.find(params[:id])
+      @reservation.status = "refusée"
+      @reservation.save
+      redirect_to owner_reservations_path
+  end
 end
